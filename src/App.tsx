9176d30ab5bdf5a90f5b6ecdd06f3a14b48b3bea
Sushma Chaudhary <susmachaudhary04@gmail.com>
2021-10-18 12:36:38 +0545

    react-query with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type Response = {
+  config: object;
+  data: Post[];
+  headers: object;
+  request: object;
+  status: number;
+  statusText: string;
+};
+
+const App: React.FC = () => {
+  const fetchPosts = async () => {
+    const response: Response | undefined = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return response?.data;
+  };
+
+  const { isLoading, error, data } = useQuery<Post[] | undefined>(
+    "dogs",
+    fetchPosts
   );
-}
+
+  if (error) return <h1> Error, try again</h1>;
+  if (isLoading) return <h1> Loading ...</h1>;
+
+  console.log(data);
+
+  return <div>{data && data.map((item) => item.title)}</div>;
+};
 
 export default App;
